cc20d51463b4f69a1613e313fb90db61fecc4341
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-31 13:28:36 -0300

    Agregar modelo Tag, controlador TagController y migraciones para la gestión de etiquetas en trabajos; actualizar fábricas y pruebas para incluir nuevas relaciones.

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Testing\Fakes\Fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
@@ -17,7 +19,13 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => fake()->jobTitle,
+            'salary' => fake()->randomElement(['$15.000.000', '$30.000.000', '$50.000.000', '70.000.000']),
+            'location' => fake()->randomElement(['Remoto', 'Presencial', 'Hibrido']),
+            'schedule' => fake()->randomElement(['Full time', 'Freelance', 'Part time']),
+            'url' => fake()->url,
+            'featured' => fake()->boolean()
         ];
     }
 }
---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+
+it('belongs to an employer', function () {
+    // arrange, act, assert
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+it('can have tags', function () {
+    $job = Job::factory()->create();
+    $job->tag('frontend');
+    expect($job->tags)->toHaveCount(1);
+});
