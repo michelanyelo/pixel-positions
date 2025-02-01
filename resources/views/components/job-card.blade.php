32b874fb4f070cd4745dc0c4a4be30aba15b752d
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-01 16:31:05 -0300

    Agregar enlaces a las tarjetas de trabajo que dirigen a la URL del trabajo.

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -6,16 +6,18 @@
     </div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 transition duration-300 text-xl font-bold">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-600 transition duration-300 text-xl font-bold">
+            <a href="{{$job->url}}" target="_blank">{{ $job->title }}</a>
+        </h3>
         <p class="text-sm mt-4">{{ $job->schedule }} anual: {{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div class="space-x-1.5">
             @foreach ($job->tags as $tag)
-                <x-tag :$tag size="small" />
+                <x-tag :$tag size="small"/>
             @endforeach
         </div>
-        <x-employer-logo :width="42" />
+        <x-employer-logo :width="42"/>
     </div>
 </x-panel>
